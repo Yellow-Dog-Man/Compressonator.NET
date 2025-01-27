9f6bb831d19edd5eb305647b411f13764229d540
Richard Fox <richard.fox@ahref.co.uk>
2025-01-27 13:52:26 -0800

    :(

--- Compressonator.NET.Samples/Program.cs
+++ Compressonator.NET.Samples/Program.cs
@@ -6,5 +6,7 @@ public class Program
     {
         Console.WriteLine($"SDK Supported: " + SDK_NativeMethods.IsSupported);
         Console.WriteLine($"Framwork Supported: " + FrameworkNativeMethods.IsSupported);
+
+        //TODO: I tried for like 3 hours to get a texture conversion/compression working but I have no idea what I'm doing - Prime.
     }
 }
