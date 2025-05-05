3e27bb66db717b2577b1e44b115ff1b87da047e4
Richard Fox <richard.fox@ahref.co.uk>
2025-05-04 18:13:51 -0700

    tweak(tests): orchestrate common CMP steps into helper class.

--- Compressonator.NET.Tests/Snapshot/RoundTrip.cs
+++ Compressonator.NET.Tests/Snapshot/RoundTrip.cs
@@ -6,19 +6,11 @@ public class RoundTrip : SnapshotTestingBase
     [TestMethod]
     public async Task BasicRoundTrip()
     {
-        string sourceFile = CurrentFile.Relative("./Resources/rainbow.png");
-        string targetFile = CurrentFile.Relative("./Out/rainbow.png");
+        string sourceFile = "./Resources/rainbow.png";
+        string targetFile = "./Out/rainbow.png";
 
-        CMP_MipSet mipSetIn = new();
-        var cmpStatus = FrameworkNativeMethods.CMP_LoadTexture(sourceFile, mipSetIn);
-        Assert.AreEqual(CMP_ERROR.CMP_OK, cmpStatus);
+        var (res,set) = SnapshotUtilities.Load(sourceFile);
 
-        cmpStatus = FrameworkNativeMethods.CMP_SaveTexture(targetFile, mipSetIn);
-
-        Assert.AreEqual(CMP_ERROR.CMP_OK, cmpStatus);
-
-        await VerifyFile(targetFile);
-
-        File.Delete(targetFile); //Clean-up
+        await SnapshotUtilities.SaveVerifyDelete(targetFile, set);
     }
 }
---
+++ Compressonator.NET.Tests/Snapshot/SnapshotUtilities.cs
@@ -0,0 +1,37 @@
+﻿
+
+using System.Net.NetworkInformation;
+
+namespace Compressonator.NET.Tests.Snapshot;
+
+public static class SnapshotUtilities
+{
+    /// <summary>
+    /// Standardized way to create the start of a compressonator chain for testing.
+    /// </summary>
+    /// <param name="relativePath"></param>
+    /// <returns></returns>
+    public static (CMP_ERROR, CMP_MipSet) Load(string relativePath)
+    {
+        var path = CurrentFile.Relative(relativePath);
+        CMP_MipSet mipSetIn = new();
+        var cmpStatus = FrameworkNativeMethods.CMP_LoadTexture(path, mipSetIn);
+        Assert.AreEqual(CMP_ERROR.CMP_OK, cmpStatus);
+
+        return (cmpStatus, mipSetIn);
+    }
+
+    public static async Task SaveVerifyDelete(string relativePath, CMP_MipSet set)
+    {
+        var path = CurrentFile.Relative(relativePath);
+        var cmpStatus = FrameworkNativeMethods.CMP_SaveTexture(path, set);
+
+        Assert.AreEqual(CMP_ERROR.CMP_OK, cmpStatus);
+
+        VerifyResult? res = await VerifyFile(path);
+        Assert.IsNotNull(res);
+        Assert.IsNull(res.Target);
+
+        File.Delete(path); //Clean-up
+    }
+}
