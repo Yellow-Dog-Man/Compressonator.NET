d4d99eac37abc4bf14bbacf02bc6b36948b98bab
Richard Fox <richard.fox@ahref.co.uk>
2025-06-19 06:36:38 -0700

    tests(BC67): add multi-thread and single thread tests.

--- Compressonator.NET.Tests/Snapshot/CompressionTests.cs
+++ Compressonator.NET.Tests/Snapshot/CompressionTests.cs
@@ -8,8 +8,10 @@ public class CompressionTests : SnapshotTestingBase
     [DataRow(CMP_FORMAT.BC3, "Resources/rainbow.png")]
     [DataRow(CMP_FORMAT.BC4, "Resources/rainbow.png")]
     [DataRow(CMP_FORMAT.BC5, "Resources/rainbow.png")]
-    [DataRow(CMP_FORMAT.BC7, "Resources/rainbow.png", 0.05f)]
+    [DataRow(CMP_FORMAT.BC7, "Resources/rainbow.png", 0.05f, 1)]
     [DataRow(CMP_FORMAT.BC6H, "Resources/rainbow.png", 0.05f, 1)]
+    [DataRow(CMP_FORMAT.BC7, "Resources/rainbow.png", 0.05f, 2)]
+    [DataRow(CMP_FORMAT.BC6H, "Resources/rainbow.png", 0.05f, 2)]
     [DataTestMethod]
     public async Task TestCompression(CMP_FORMAT targetFormat, string inputFileRelativePath, float quality = 0.9f, int maxThreads = 0)
     {
