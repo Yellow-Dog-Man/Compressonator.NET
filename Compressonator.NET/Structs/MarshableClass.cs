63aa97254efb74402dca747be6cc276358f87bde
Richard Fox <richard.fox@ahref.co.uk>
2025-06-23 10:15:50 -0700

    feat(marshal): provide reference marshal implementation.
    Don't use it, it will break you if you inherit from it

---
+++ Compressonator.NET/Structs/MarshableClass.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Compressonator.NET
+{
+    //TODO: When we can drop, earlier .Nets we can look into:
+    //https://learn.microsoft.com/en-us/dotnet/standard/native-interop/custom-marshalling-source-generation
+    [Obsolete("DO NOT Inherit THIS IT WILL NOT WORK, Use it as a template to make your formatted classes work")]
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    internal abstract class MarshableClass : IDisposable
+    {
+        public abstract int dataSize { get; }
+
+        private IntPtr unmanagedPtr = IntPtr.Zero;
+        internal IntPtr UnmanagedCopy
+        {
+            get
+            {
+                bool hasData = false;
+                if (unmanagedPtr == IntPtr.Zero)
+                    unmanagedPtr = Marshal.AllocHGlobal(dataSize);
+                else
+                    hasData = true;
+
+                Marshal.StructureToPtr(this, unmanagedPtr, hasData);
+
+                return unmanagedPtr;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (unmanagedPtr != IntPtr.Zero)
+                    Marshal.FreeHGlobal(unmanagedPtr);
+                disposed = true;
+            }
+        }
+
+        ~MarshableClass()
+        {
+            Dispose(false);
+        }
+    }
+}
