ecaad1e19c3483ee2921fdfa8acc5779794fd2d4
Richard Fox <richard.fox@ahref.co.uk>
2025-06-23 19:59:10 -0700

    docs(marshal): explain why marshal tests exist

--- Compressonator.NET.Tests/MarshalTests.cs
+++ Compressonator.NET.Tests/MarshalTests.cs
@@ -5,6 +5,8 @@ namespace Compressonator.NET.Tests;
 [TestClass]
 public class MarshalTests
 {
+    // These are manually calculated from the C++ code, if they change, you will need to get them.
+    // Usually sizeof(CMP_CompressOptions), I dont see this changing much.
     public const int CORRECT_COMPRESSOPTIONS_SIZE = 1523;
     public const int CORRECT_FORMAT_SIZE = 4;
 
