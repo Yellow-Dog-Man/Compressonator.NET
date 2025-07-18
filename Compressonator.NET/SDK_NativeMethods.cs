80e8f6fbc11fabbafc196733ebfa487f24f8646c
Richard Fox <richard.fox@ahref.co.uk>
2025-07-18 07:28:18 -0700

    tweak(CMP_ConverTexture): tweak usage of options ptr
    This makes debugger stepping easier, trust me :)

--- Compressonator.NET/SDK_NativeMethods.cs
+++ Compressonator.NET/SDK_NativeMethods.cs
@@ -16,8 +16,8 @@ namespace Compressonator.NET
 
         public static CMP_ERROR CMP_ConvertTexture(CMP_Texture sourceTexture, CMP_Texture destTexture, CMP_CompressOptions options)
         {
-            var ptr = new MarshaledStruct<CMP_CompressOptions>();
-            return CMP_ConvertTexture(sourceTexture, destTexture, ptr.Write(options), IntPtr.Zero);
+            var compressOptionsPtr = new MarshaledStruct<CMP_CompressOptions>().Write(options);
+            return CMP_ConvertTexture(sourceTexture, destTexture, compressOptionsPtr, IntPtr.Zero);
         }
 
         [DllImport(LIBRARY_NAME, CallingConvention = CallingConvention.Cdecl)]
