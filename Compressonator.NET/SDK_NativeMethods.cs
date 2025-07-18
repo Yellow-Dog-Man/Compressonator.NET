e3132cef4bd6f4ceb3acf54708b0227383e6db01
Richard Fox <richard.fox@ahref.co.uk>
2025-07-18 07:29:42 -0700

    feat(mips): expose CMP_MipSetToTexture from native
    NEEDS NEW NATIVES

--- Compressonator.NET/SDK_NativeMethods.cs
+++ Compressonator.NET/SDK_NativeMethods.cs
@@ -33,6 +33,8 @@ namespace Compressonator.NET
         [DllImport(LIBRARY_NAME, CallingConvention = CallingConvention.Cdecl)]
         public static extern bool CMP_IsValidFormat([In]CMP_FORMAT format);
 
+        [DllImport(LIBRARY_NAME, CallingConvention = CallingConvention.Cdecl)]
+        public static extern CMP_ERROR CMP_MipSetToTexture([In][Out] CMP_MipSet mipSetIn, int mipLevel,[In][Out] CMP_Texture destTexture);
         public static bool IsSupported => _isSupported.Value;
 
         static readonly Lazy<bool> _isSupported = new Lazy<bool>(() =>
