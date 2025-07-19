9af0a704d854d7540f37fecc226ad12aefe8f756
Richard Fox <richard.fox@ahref.co.uk>
2025-07-18 23:07:43 -0700

    fix(tests): add CompressDecompress test.
    This proves decompression!

--- Compressonator.NET.Tests/Snapshot/RoundTrip.cs
+++ Compressonator.NET.Tests/Snapshot/RoundTrip.cs
@@ -1,4 +1,6 @@
-﻿namespace Compressonator.NET.Tests.Snapshot;
+﻿using Compressonator.NET.Utilities;
+
+namespace Compressonator.NET.Tests.Snapshot;
 
 [TestClass]
 public class RoundTrip : SnapshotTestingBase
@@ -7,10 +9,56 @@ public class RoundTrip : SnapshotTestingBase
     public async Task BasicRoundTrip()
     {
         string sourceFile = "Resources/rainbow.png";
-        var (res,set) = SnapshotUtilities.Load(sourceFile, CompressionTests.DEFAULT_SOURCE_FORMAT, CompressionTests.DEFAULT_SOURCE_FORMAT);
-
-        Assert.AreEqual(CMP_FORMAT.RGBA_8888, set.format);
+        var (res,set) = SnapshotUtilities.Load(sourceFile);
 
         await SnapshotUtilities.SaveVerifyDelete(set, "png");
+    }
+
+    [TestMethod]
+    public async Task CompressDecompressValidate()
+    {
+        CMP_ERROR cmpStatus = CMP_ERROR.CMP_OK;
+
+        string sourceFile = "Resources/carrots.png";
+        var (res, originalSet) = SnapshotUtilities.Load(sourceFile, mipLevels:0);
+
+        CMP_MipSet compressedSet = new();
+        compressedSet.format = CMP_FORMAT.BC1;
+        var options = new CMP_CompressOptions()
+        {
+            destFormat = compressedSet.format,
+            sourceFormat = originalSet.format,
+        };
+
+        // Compress to BC1
+        cmpStatus = SDK_NativeMethods.CMP_ConvertMipTexture(originalSet, compressedSet, options);
+        Assert.AreEqual(CMP_ERROR.CMP_OK, cmpStatus);
+        
+        // Extract mip level 0
+        CMP_Texture mipZeroTexture = new();
+
+        cmpStatus = SDK_NativeMethods.CMP_MipSetToTexture(compressedSet, 0, mipZeroTexture);
+        Assert.AreEqual(CMP_ERROR.CMP_OK, cmpStatus);
+
+        // Immediately turn around and re-decompress it
+        CMP_Texture decompressed = new();
+
+        TextureUtilities.CopyDimensions(mipZeroTexture, decompressed);
+        decompressed.format = SnapshotUtilities.DEFAULT_SOURCE_FORMAT;
+        decompressed.transcodeFormat = SnapshotUtilities.DEFAULT_SOURCE_FORMAT;
+        decompressed.AllocateDataPointer();
+
+        Assert.AreEqual(CMP_ERROR.CMP_OK, cmpStatus);
+
+        options = new CMP_CompressOptions()
+        {
+            destFormat = SnapshotUtilities.DEFAULT_SOURCE_FORMAT,
+        };
+
+        cmpStatus = SDK_NativeMethods.CMP_ConvertTexture(mipZeroTexture, decompressed, options);
+        Assert.AreEqual(CMP_ERROR.CMP_OK, cmpStatus);
+
+        // Standard Save Verify Delete
+        await SnapshotUtilities.SaveVerifyDelete(decompressed, "png");
     }
 }
